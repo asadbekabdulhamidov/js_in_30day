78b064637d674b2a7f07e7496a1ea6757bc00abf
abdulkhamidov <aabdulkhamidov818@gmail.com>
2024-09-07 13:53:05 +0500

    copy btn

--- 11-amaliyot_Pretty-JSON-formatter/js/app.js
+++ 11-amaliyot_Pretty-JSON-formatter/js/app.js
@@ -2,7 +2,7 @@ const inputJson = document.querySelector("#inputJson");
 const outputJson = document.querySelector("#outputJson");
 const formatted = document.querySelector("#formatted");
 const minifed = document.querySelector("#minifed");
-const copy = document.querySelector(".inputJson > svg");
+const copy = document.querySelector(".input-json > svg");
 
 formatted.addEventListener("click", () => {
   outputJson.value = JSON.stringify(JSON.parse(inputJson.value), null, 4);
