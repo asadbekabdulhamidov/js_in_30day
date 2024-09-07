506d9583bd669f2f2006948a114b5916b23eefd2
abdulkhamidov <aabdulkhamidov818@gmail.com>
2024-09-07 13:50:51 +0500

    json formatter

---
+++ 11-amaliyot_Pretty-JSON-formatter/js/app.js
@@ -0,0 +1,17 @@
+const inputJson = document.querySelector("#inputJson");
+const outputJson = document.querySelector("#outputJson");
+const formatted = document.querySelector("#formatted");
+const minifed = document.querySelector("#minifed");
+const copy = document.querySelector(".inputJson > svg");
+
+formatted.addEventListener("click", () => {
+  outputJson.value = JSON.stringify(JSON.parse(inputJson.value), null, 4);
+});
+
+minifed.addEventListener("click", () => {
+  outputJson.value = JSON.stringify(JSON.parse(inputJson.value));
+});
+
+copy.addEventListener("click", () => {
+  navigator.clipboard.writeText(outputJson.value);
+});
